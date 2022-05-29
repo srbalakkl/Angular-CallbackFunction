bb55a31d2853aa3c10940b0d64e0c97b69be2008
bala <sr.bala.kkl@gmail.com>
2022-05-29 20:43:00 +0530

    call back Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-CallbackFunction';
+
+  ngOnInit() {
+    console.log(1);
+    this.displayValue(10,5,function (arg: any){
+      console.log('call back function value is '+arg)
+    });
+    // console.log(result);
+    console.log(3);
+  }
+
+  displayValue(a: number, b: number,callbackfn:any){
+    setTimeout(() =>{
+      callbackfn (a + b);
+      },1000
+    )
+  }
 }
