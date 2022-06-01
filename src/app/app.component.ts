26ec19937a9749c1c69f555fa37f67f627fe0041
bala <sr.bala.kkl@gmail.com>
2022-06-01 09:03:12 +0530

    call back Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,30 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Angular-CallbackFunction';
-
+/**@link https://gist.github.com/288cebe9b828ec16e542d097ae29afce
+ * git hub link for promise
+ * */
   ngOnInit() {
     console.log(1);
     this.displayValue(10,5,function (arg: any){
       console.log('call back function value is '+arg)
     });
-    // console.log(result);
     console.log(3);
+    this.anotherEgForCallbackFn(()=>{console.log('cb fn is called')})
   }
 
   displayValue(a: number, b: number,callbackfn:any){
     setTimeout(() =>{
       callbackfn (a + b);
       },1000
-    )
+    );
+  }
+
+  anotherEgForCallbackFn(cb:any){
+    setTimeout(()=>{
+      console.log('Asunc task');
+      cb();
+    },5000);
+  //  this cd() fn is called after 5sec asynchronously
   }
 }
